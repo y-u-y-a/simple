4bca72dd0dfc39627412d2348ae4d9547d596a49
y-u-y-a <yuya.ymd00@gmail.com>
2020-07-17 16:21:54 +0900

    コメントの修正など

--- archive.php
+++ archive.php
@@ -1,18 +1,16 @@
 
-<!-- ヘッダー ======================== -->
 <?php get_header(); ?>
-<!-- メイン ========================= -->
+
 <main class="clearfix">
-<!-- 一覧ページの場合のタイトル -->
-  <?php if(is_category()): ?>  <!-- (カテゴリーページならば) -->
+<!-- アーカイブページ -->
+  <?php if (is_category() ): ?><!-- (カテゴリ一覧) -->
     <p class="archive-title">ー <?php single_cat_title(); ?>に関する記事一覧 ー</p>
-  <?php elseif(is_month()): ?><!-- (月別ページならば) -->
+  <?php elseif (is_month() ): ?><!-- (月別一覧) -->
     <p class="archive-title"> ー<?php echo get_the_date("Y年n月") ?>の記事一覧 ー</p>
   <?php endif; ?>
-  <!-- 記事ページの内容を挿入 -->
+  <!-- article.php -->
   <?php get_template_part("article"); ?>
-  <!-- サイドバー -->
   <?php get_sidebar(); ?>
 </main>
-<!-- フッター ======================= -->
+
 <?php get_footer(); ?>
--- home.php
+++ home.php
@@ -1,12 +1,10 @@
 
-<!-- ヘッダー ======================== -->
 <?php get_header(); ?>
-<!-- メイン ========================= -->
+
 <main class="clearfix">
-  <!-- 記事ページの内容を挿入 -->
+  <!-- article.php(記事) -->
   <?php get_template_part("article"); ?>
-  <!-- サイドバー -->
   <?php get_sidebar(); ?>
 </main>
-<!-- フッター ======================= -->
+
 <?php get_footer(); ?>
